1f2c2b977e43022b7657d35f496dfca4fe002693
NilsDevos <nils.devos@ordina.be>
2019-08-12 10:48:45 +0200

    added build files / added scripts to package.json

---
+++ builder.js
@@ -0,0 +1,30 @@
+const {spawn} = require('child_process');
+const config = process.env.NODE_ENV ? `-c=${process.env.NODE_ENV}` : '';
+
+console.log(`[BUILDER]\tBuilding application with configuration: ${config ? process.env.NODE_ENV : 'default'}`);
+
+let prebuildScript = `npm run prebuild`;
+if (process.env.SKIP_PREBUILD) {
+  prebuildScript = `echo "Skipping prebuild"`;
+}
+
+const prebuild = spawn(prebuildScript, {
+  shell: true
+});
+
+prebuild.stdout.pipe(process.stdout);
+prebuild.stderr.pipe(process.stdout);
+
+prebuild.on('close', (code) => {
+  console.log(`[BUILDER]\tprebuild process exited with code ${code}`);
+  const build = spawn(`ng build ${process.env.NODE_ENV === 'prod' ? '--prod' : ''} ${config} `, {
+    shell: true
+  });
+  build.stdout.pipe(process.stdout);
+  build.stderr.pipe(process.stdout);
+  build.on('close', async (buildCode) => {
+    console.log(`[BUILDER]\tbuild process exited with code ${buildCode}`);
+    process.exit(code ? code : buildCode);
+  });
+});
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const frameguard = require('frameguard');
+var helmet = require('helmet');
+app.disable('x-powered-by');
+app.use(helmet());
+app.use(helmet.featurePolicy({
+  features: {
+    fullscreen: ["'self'"],
+    vibrate: ["'none'"],
+    payment: ["'none'"],
+    syncXhr: ["'none'"],
+    push: ["'none'"]
+  }
+}));
+// Sets "Strict-Transport-Security: max-age=5184000; includeSubDomains".
+const sixtyDaysInSeconds = 5184000;
+app.use(helmet.hsts({
+  maxAge: sixtyDaysInSeconds
+}));
+
+app.use('/', express.static(`${__dirname}/dist/vertical-foundations`));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(`${__dirname}/dist/vertical-foundations/index.html`));
+});
+app.use(frameguard({action: 'sameorigin'}));
+app.listen(process.env.PORT || 8080, function () {
+  console.log(`Listening at http://localhost:${process.env.PORT || 8080}`);
+});
