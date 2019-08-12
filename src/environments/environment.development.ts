4ae97f85a0d9c82491ce3a2d79a017d66837e394
NilsDevos <nils.devos@ordina.be>
2019-08-12 12:41:05 +0200

    added development environment file

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import {PublicLayoutComponent} from '../app/layout/smartmonitoring/public.layout.component';
+import {PrivateLayoutComponent} from '../app/layout/smartmonitoring/private.layout.component';
+import {PublicAuthGuard} from '../app/app.routing';
+
+export const environment = {
+  production: false,
+  baseUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/api/',
+  loginUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/auth/login',
+  authUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/auth/',
+  assetPrefix: 'smartmonitoring',
+
+  paths: {
+    home: './pages/devtest/devtest.module#DevtestModule'
+  }
+
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://www.proximus.be/smartapps/smartmonitoring/api/',
+  loginUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/login',
+  authUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/',
+  assetPrefix: 'smartmonitoring',
 };
