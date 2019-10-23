3988b962f88c360d8ab081703fa6b7ace5d28431
NilsDevos <nils.devos@ordina.be>
2019-10-23 14:32:50 +0200

    Small change to config

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/api',
+  baseUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/api/',
   loginUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/login',
   authUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/',
   assetPrefix: 'smartmonitoring',
