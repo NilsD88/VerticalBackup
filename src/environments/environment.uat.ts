dcb6e0ed7ffdddf47cb6401542997b0f0314aafa
NilsDevos <nils.devos@ordina.be>
2019-10-23 14:15:16 +0200

    uat config

---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,12 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: true,
+  baseUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/api',
+  loginUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/login',
+  authUrl: 'https://www.proximus.be/smartapps/smartmonitoring/auth/',
+  assetPrefix: 'smartmonitoring',
+};
