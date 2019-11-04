b26771109b0bf4075c7bb62e882d6200f1829be6
NilsDevos <nils.devos@ordina.be>
2019-11-04 11:09:57 +0100

    corrected uat environment file

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: true,
   baseUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/v2/api',
-  loginUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/v2/auth/login',
-  authUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/v2/auth/',
+  loginUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/auth/login',
+  authUrl: 'https://www-uat.proximus.be/smartapps/smartmonitoring/auth/',
   assetPrefix: 'smartmonitoring',
   paths: {
     home: './pages/devtest/devtest.module#DevtestModule'
