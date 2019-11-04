e4664324877357f6d7e712badd0d55c22a96c767
NilsDevos <nils.devos@ordina.be>
2019-11-04 13:57:35 +0100

    fixed typo

--- src/app/models/g-asset.model.ts
+++ src/app/models/g-asset.model.ts
@@ -1,56 +1,56 @@
-import { ILocation } from './g-location.model';
-import { IGeolocation } from './geolocation.model';
-import { IThresholdTemplate } from './g-threshold-template.model';
-import { IThing } from './g-thing.model';
-import { isNullOrUndefined } from 'util';
-import { IAlert } from './g-alert.model';
+import {ILocation} from './g-location.model';
+import {IGeolocation} from './geolocation.model';
+import {IThresholdTemplate} from './g-threshold-template.model';
+import {IThing} from './g-thing.model';
+import {isNullOrUndefined} from 'util';
+import {IAlert} from './g-alert.model';
 
 export interface IAsset {
-    id?: string;
-    name?: string;
-    description?: string;
-    location?: ILocation;
-    locationId?: string;
-    geolocation?: IGeolocation;
-    image?: string;
-    thresholdTemplate?: IThresholdTemplate;
-    thresholdTemplateId?: string;
-    things?: IThing[];
-    thingsId?: string[];
-    alerts?: IAlert[];
+  id?: string;
+  name?: string;
+  description?: string;
+  location?: ILocation;
+  locationId?: string;
+  geolocation?: IGeolocation;
+  image?: string;
+  thresholdTemplate?: IThresholdTemplate;
+  thresholdTemplateId?: string;
+  things?: IThing[];
+  thingIds?: string[];
+  alerts?: IAlert[];
 
-    lastAlert?: any;
+  lastAlert?: any;
 }
 
 export class Asset {
-    id: string;
-    name: string;
-    description: string;
-    location: ILocation;
-    locationId: string;
-    geolocation: IGeolocation;
-    image: string;
-    thresholdTemplate: IThresholdTemplate;
-    thresholdTemplateId: string;
-    things: IThing[] = [];
-    thingsId: string[];
+  id: string;
+  name: string;
+  description: string;
+  location: ILocation;
+  locationId: string;
+  geolocation: IGeolocation;
+  image: string;
+  thresholdTemplate: IThresholdTemplate;
+  thresholdTemplateId: string;
+  things: IThing[] = [];
+  thingsId: string[];
 
-    lastAlert: any;
+  lastAlert: any;
 
-    constructor(asset: IAsset = null) {
-        if (asset) {
-            for (const key in asset) {
-                if (!isNullOrUndefined(key)) {
-                    this[key] = asset[key];
-                }
-            }
+  constructor(asset: IAsset = null) {
+    if (asset) {
+      for (const key in asset) {
+        if (!isNullOrUndefined(key)) {
+          this[key] = asset[key];
         }
+      }
     }
+  }
 }
 
 export interface IPagedAssets {
-    assets?: IAsset[];
-    pageNumber?: number;
-    totalElements?: number;
-    totalPages?: number;
+  assets?: IAsset[];
+  pageNumber?: number;
+  totalElements?: number;
+  totalPages?: number;
 }
