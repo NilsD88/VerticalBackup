d20d9291a2dfa8259882f5a8bab9e2d9ed1eacb2
3mad <Tlmdz13@hotmail.com>
2020-01-06 15:49:05 +0100

    added missing files

---
+++ src/app/pages/stairway-to-health/place/place.routing.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+import { PlaceComponent } from './place.component';
+
+export const PlaceRoutes: Routes = [
+  {
+    path: '',
+    component: PlaceComponent
+  }
+];
