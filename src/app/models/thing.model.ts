836fd310fbb7d3ef9a0a847e70650ca161b92bd1
NilsDevos <nils.devos@ordina.be>
2020-03-26 15:25:39 +0100

    forgotten import

--- src/app/models/thing.model.ts
+++ src/app/models/thing.model.ts
@@ -1,10 +1,11 @@
-import { ISensor } from './sensor.model';
+import {ISensor} from './sensor.model';
+import {IAsset} from './asset.model';
 
 export interface IThing {
-    id?: string;
-    devEui?: string;
-    name?: string;
-    batteryPercentage?: number;
-    sensors?: ISensor[];
-    assets?: IAsset[];
+  id?: string;
+  devEui?: string;
+  name?: string;
+  batteryPercentage?: number;
+  sensors?: ISensor[];
+  assets?: IAsset[];
 }
