420d9f19ff3a384e14e3283af326dd936138f859
NilsDevos <nils.devos@ordina.be>
2020-03-26 15:41:23 +0100

    translate import was gone

--- src/app/pages/walking-trail/dashboard/dashboard.module.ts
+++ src/app/pages/walking-trail/dashboard/dashboard.module.ts
@@ -1,14 +1,15 @@
-import { LocationExplorerModule } from 'projects/ngx-proximus/src/lib/location-explorer/location-explorer.module';
-import { MatButtonToggleModule, MatIconModule } from '@angular/material';
-import { WalkingTrailMapModule } from '../lib/map/map.module';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
-import { DashboardRoutes } from './dashboard.routing';
-import { CountPastWeekModule } from 'src/app/shared/people-counting/dashboard/charts/count-past-week/count-past-week.module';
-import { TotalCountPastWeekModule } from 'src/app/shared/people-counting/dashboard/charts/total-count-past-week/total-count-past-week.module';
-import { TotalCountPastYearModule } from 'src/app/shared/people-counting/dashboard/charts/total-count-past-year/total-count-past-year.module';
+import {LocationExplorerModule} from 'projects/ngx-proximus/src/lib/location-explorer/location-explorer.module';
+import {MatButtonToggleModule, MatIconModule} from '@angular/material';
+import {WalkingTrailMapModule} from '../lib/map/map.module';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DashboardComponent} from './dashboard.component';
+import {DashboardRoutes} from './dashboard.routing';
+import {CountPastWeekModule} from 'src/app/shared/people-counting/dashboard/charts/count-past-week/count-past-week.module';
+import {TotalCountPastWeekModule} from 'src/app/shared/people-counting/dashboard/charts/total-count-past-week/total-count-past-week.module';
+import {TotalCountPastYearModule} from 'src/app/shared/people-counting/dashboard/charts/total-count-past-year/total-count-past-year.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [DashboardComponent],
@@ -22,6 +23,8 @@ import { TotalCountPastYearModule } from 'src/app/shared/people-counting/dashboa
     CountPastWeekModule,
     TotalCountPastWeekModule,
     TotalCountPastYearModule,
+    TranslateModule,
   ]
 })
-export class DashboardModule {}
+export class DashboardModule {
+}
