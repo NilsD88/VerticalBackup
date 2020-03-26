2619c335e610353843fecb35cb4b5114b2c21d87
NilsDevos <nils.devos@ordina.be>
2020-03-26 16:29:43 +0100

    one more translation module

--- src/app/pages/people-counting-retail/store/store.module.ts
+++ src/app/pages/people-counting-retail/store/store.module.ts
@@ -1,16 +1,17 @@
-import { AssetExplorerModule } from 'projects/ngx-proximus/src/lib/asset-explorer/asset-explorer.module';
-import { AssetsCounterModule } from 'src/app/shared/people-counting/location/assets-counter/assets-counter.module';
-import { CountByAssetModule } from 'src/app/shared/people-counting/location/charts/count-by-asset/count-by-asset.module';
-import { CalendarViewModule } from './../../../shared/people-counting/location/charts/calendar-view/calendar-view.module';
-import { MapModule } from 'projects/ngx-proximus/src/lib/map/map.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StoreComponent } from './store.component';
-import { RouterModule } from '@angular/router';
-import { StoreRoutes } from './store.routing';
-import { MonthViewModule } from 'src/app/shared/people-counting/location/charts/month-view/month-view.module';
-import { DayViewModule } from 'src/app/shared/people-counting/location/charts/day-view/day-view.module';
-import { SummaryModule } from 'src/app/shared/people-counting/location/summary/summary.module';
+import {AssetExplorerModule} from 'projects/ngx-proximus/src/lib/asset-explorer/asset-explorer.module';
+import {AssetsCounterModule} from 'src/app/shared/people-counting/location/assets-counter/assets-counter.module';
+import {CountByAssetModule} from 'src/app/shared/people-counting/location/charts/count-by-asset/count-by-asset.module';
+import {CalendarViewModule} from './../../../shared/people-counting/location/charts/calendar-view/calendar-view.module';
+import {MapModule} from 'projects/ngx-proximus/src/lib/map/map.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StoreComponent} from './store.component';
+import {RouterModule} from '@angular/router';
+import {StoreRoutes} from './store.routing';
+import {MonthViewModule} from 'src/app/shared/people-counting/location/charts/month-view/month-view.module';
+import {DayViewModule} from 'src/app/shared/people-counting/location/charts/day-view/day-view.module';
+import {SummaryModule} from 'src/app/shared/people-counting/location/summary/summary.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
@@ -26,8 +27,10 @@ import { SummaryModule } from 'src/app/shared/people-counting/location/summary/s
     MapModule,
     SummaryModule,
     AssetExplorerModule,
-    DayViewModule
+    DayViewModule,
+    TranslateModule
   ],
   exports: [StoreComponent]
 })
-export class StoreModule { }
+export class StoreModule {
+}
