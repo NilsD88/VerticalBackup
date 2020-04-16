dc60e92220941122d262c8c16e72da56432bf73b
NilsDevos <nils.devos@ordina.be>
2020-04-16 11:23:55 +0200

    fixed merge and removed nothing closing div tag

--- src/app/pages/walking-trail/trail/trail-map/trail-map.module.ts
+++ src/app/pages/walking-trail/trail/trail-map/trail-map.module.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { IconModule } from './../../../../../../projects/ngx-proximus/src/lib/icon/icon.module';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { TrailMapComponent } from './trail-map.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { LeafletMarkerClusterModule } from '@asymmetrik/ngx-leaflet-markercluster';
-
-=======
 import {IconModule} from './../../../../../../projects/ngx-proximus/src/lib/icon/icon.module';
 import {RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
@@ -17,8 +6,6 @@ import {TrailMapComponent} from './trail-map.component';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {LeafletMarkerClusterModule} from '@asymmetrik/ngx-leaflet-markercluster';
 import {TranslateModule} from '@ngx-translate/core';
->>>>>>> uat-config
-
 
 @NgModule({
   declarations: [TrailMapComponent],
